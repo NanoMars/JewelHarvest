6305c4914bc16ca142067d08a060f8666a87fdfd
Armand Packham-McGuiness <armand.packham@gmail.com>
2024-06-30 10:54:38 +1000

    Created a testing script to play with sprites, and defined a sprite

---
+++ SpriteExample.py
@@ -0,0 +1,38 @@
+import sys
+ 
+import pygame
+from pygame.locals import *
+ 
+pygame.init()
+ 
+fpsClock = pygame.time.Clock()
+sprites = pygame.sprite.Group()
+
+width, height = 1280, 720
+screen = pygame.display.set_mode((width, height))
+
+class Player(pygame.sprite.Sprite): 
+  #define the sprite for the player
+  def __init__(self):
+    pygame.sprite.Sprite.__init__(self)
+    self.image = pygame.Surface((50,50))
+    self.image.fill((255,0,255))
+    self.rect = self.image.get_rect()
+    self.rect.center = (width / 2, height / 2)
+    
+
+# Game loop.
+while True:
+  fpsClock.tick(fps)
+  for event in pygame.event.get():
+    if event.type == QUIT:
+      pygame.quit()
+      sys.exit()
+  
+  # Update.
+  sprites.update()
+  
+  # Draw.
+  sprites.draw(screen)
+  screen.fill((15, 15, 25))
+  pygame.display.flip()
